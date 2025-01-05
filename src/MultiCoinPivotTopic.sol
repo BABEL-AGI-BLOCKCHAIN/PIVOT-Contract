0b61809dcaab90b2a3956b1217476a81d969d074
ciconia11 <18729565508@163.com>
2025-01-05 22:32:32 +0800

    fix topic contract and add multicoin contract

--- src/MultiCoinPivotTopic.sol
+++ src/MultiCoinPivotTopic.sol
@@ -8,18 +8,19 @@ contract PivotTopic {
     address public owner;
     address public sbtAddress;
     ISBTContract public sbtContract;
-    IERC20 public erc20Contract;
     uint256 private _topicId;
     uint16  private _commissionrate = 3;
-    uint256 public _totalBalance;
-    uint256 public _totalCommission;
-
+    
+    mapping (uint256 topicId => uint256) public _totalCommission;
+    mapping (uint256 topicId => uint256) public _totalBalance;
     mapping (uint256 topicId => address) private _promoter;
-    mapping (address owner => uint256) private _income;
-    mapping (address owner => uint256) private _investment;
+    mapping (address investor => mapping(uint256 topicId => uint256)) private _income;
+    mapping (address investor => mapping(uint256 topicId => uint256)) private _investment;
     mapping (uint256 topicId => uint256) private _fixedInvestment;
     mapping (uint256 topicId => uint256) private _position;
     mapping (uint256 topicId => mapping(uint256 position => address)) private _investAddressMap;
+
+    mapping (uint256 topicId => address) public topicCoin;
 
     event CreateTopic(address indexed promoter, uint256 topicId, uint256 investment);
     event Invest(address indexed investor, uint256 indexed topicId, uint256 amount);
@@ -33,12 +34,12 @@ contract PivotTopic {
         _topicId = 1;
     }
 
-    function getIncome(address investor) public view returns(uint256) {
-        return _income[investor];
+    function getIncome(address investor, uint256 topicId) public view returns(uint256) {
+        return _income[investor][topicId];
     }
 
-    function getInvestment(address investor) public view returns(uint256) {
-        return _investment[investor];
+    function getInvestment(address investor, uint256 topicId) public view returns(uint256) {
+        return _investment[investor][topicId];
     }
 
     function getFixedInvestment(uint256 topicId) public view returns(uint256) {
@@ -49,12 +50,8 @@ contract PivotTopic {
         return _promoter[topicId];
     }
 
-    function setERC20Contract(address erc20Address) public {
-        require(msg.sender == owner, "Invalid Owner");
-        erc20Contract = IERC20(erc20Address);
-    }
 
-    function createTopic(uint256 amount) public {
+    function createTopic(uint256 amount, address erc20Address) public {
 
         address promoter = msg.sender;
 
@@ -72,9 +69,11 @@ contract PivotTopic {
 
         _position[_topicId] = position;
 
-        _topicId ++;
-
+        IERC20 erc20Contract = IERC20(erc20Address);
         erc20Contract.transfer(address(this), amount);
+        topicCoin[_topicId] = erc20Address;
+
+        _topicId ++;
 
         emit CreateTopic(promoter, _topicId, amount);
 
@@ -85,8 +84,10 @@ contract PivotTopic {
         uint256 fixedInvestment = _fixedInvestment[topicId];
         require(fixedInvestment == amount, "Insufficient Balance");
 
+        address erc20Address = topicCoin[topicId];
+        IERC20 erc20Contract = IERC20(erc20Address);
         erc20Contract.transfer(address(this), amount);
-        _totalBalance += amount;
+        _totalBalance[topicId] = _totalBalance[topicId] + amount;
 
         uint256 position = _position[topicId] + 1;
 
@@ -96,39 +97,43 @@ contract PivotTopic {
             address investAddress = _investAddressMap[topicId][i + 1];
             (bool success, uint256 income) = Math.tryDiv(fixedInvestment, position);
             require(success,"Calculate Fault");
-            _income[investAddress] = _income[investAddress] + income;
+            _income[investAddress][topicId] = _income[investAddress][topicId] + income;
         }
         _position[topicId] = position;
         emit Invest(msg.sender, topicId, fixedInvestment);
     }
 
-    function withdraw() public {
+    function withdraw(uint256 topicId) public {
 
         address to = msg.sender;
-        uint256 income = _income[to];
+        uint256 income = _income[to][topicId];
         require(income >= 0,"Insufficient Balance");
-        uint256 investment = _investment[to];
+        uint256 investment = _investment[to][topicId];
 
         if(investment < income) {
             (bool success,uint256 diff) = Math.trySub(income, investment);
             require(success,"Calculate Fault");
             uint256 commission = diff / 1000 * _commissionrate;
-            _totalCommission = _totalCommission + commission;
+            _totalCommission[topicId] = _totalCommission[topicId] + commission;
             (success,income) = Math.trySub(income, commission);
             require(success,"Calculate Fault");
         }
 
+        address erc20Address = topicCoin[topicId];
+        IERC20 erc20Contract = IERC20(erc20Address);
         erc20Contract.transferFrom(address(this), to, income);
-        _income[to] = 0;
-        _totalBalance = _totalBalance - income;
+        _income[to][topicId] = 0;
+        _totalBalance[topicId] = _totalBalance[topicId] - income;
         emit Withdraw(msg.sender, income);
     }
 
-    function withdrawCommission(uint256 amount) public {
-        require(amount < _totalCommission, "Insufficient Balance");
+    function withdrawCommission(uint256 amount, uint256 topicId) public {
+        require(amount < _totalCommission[topicId], "Insufficient Balance");
         require(msg.sender == owner, "Invalid Owner");
+        address erc20Address = topicCoin[topicId];
+        IERC20 erc20Contract = IERC20(erc20Address);
         erc20Contract.transferFrom(address(this), owner, amount);
-        _totalCommission = _totalCommission - amount;
+        _totalCommission[topicId] = _totalCommission[topicId] - amount;
         emit WithdrawCommission(owner, amount);
     }
 
